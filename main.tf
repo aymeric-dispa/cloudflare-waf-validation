71817fb1d0e7fab7e691bb5ae71ba1198f4ba04f
Aymeric <aymeric@cloudflare.com>
2026-04-21 19:15:38 +0100

    feat: enable terraform cloud backend and fix workflow

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ resource "cloudflare_ruleset" "waf_attack_score_protection" {
   kind        = "zone"
   phase       = "http_request_firewall_custom"
 
-  rules {
-    action      = var.waf_action
-    expression  = "(cf.threat_score ge ${var.waf_attack_score_threshold})"
-    description = "Block requests with high attack score (>= ${var.waf_attack_score_threshold})"
-    enabled     = true
-  }
+  rules = [
+    {
+      action      = var.waf_action
+      expression  = "(cf.threat_score ge ${var.waf_attack_score_threshold})"
+      description = "Block requests with high attack score (>= ${var.waf_attack_score_threshold})"
+      enabled     = true
+    }
+  ]
 }
 
 resource "cloudflare_ruleset" "waf_managed_rules" {
@@ -20,13 +22,15 @@ resource "cloudflare_ruleset" "waf_managed_rules" {
   kind        = "zone"
   phase       = "http_request_firewall_managed"
 
-  rules {
-    action = "execute"
-    action_parameters {
-      id = "efb7b8c949ac4650a09736fc376e9aee"
+  rules = [
+    {
+      action = "execute"
+      action_parameters = {
+        id = "efb7b8c949ac4650a09736fc376e9aee"
+      }
+      expression  = "true"
+      description = "Execute Cloudflare Managed Ruleset"
+      enabled     = true
     }
-    expression  = "true"
-    description = "Execute Cloudflare Managed Ruleset"
-    enabled     = true
-  }
+  ]
 }
